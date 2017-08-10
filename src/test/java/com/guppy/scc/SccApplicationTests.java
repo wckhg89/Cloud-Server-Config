8641e2af78024ee5206383cd3f4bd1b220cd4cf0
guppy(강홍구) <guppy.kang@kakaocorp.com>
2017-08-10 18:06:23 +0900

    init commit

---
+++ src/test/java/com/guppy/scc/SccApplicationTests.java
@@ -0,0 +1,16 @@
+package com.guppy.scc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SccApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
